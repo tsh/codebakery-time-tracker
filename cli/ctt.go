e28ec9ebb878033342df97c08f8d3571c9b537c1
tsh <dr.tallin@gmail.com>
2016-04-08 19:16:24 +0300

    #12 use flag to parse cmd args

--- cli/ctt.go
+++ cli/ctt.go
@@ -8,14 +8,33 @@ import (
 
 func main() {
 	var user string
-//	var password string
-	flag.StringVar(&user, "u", "test", "specify username to use")
+	var password string
+	var description string
+	flag.StringVar(&user, "u", "", "specify username to login on server")
+	flag.StringVar(&password, "p", "", "password to login on server")
+	flag.StringVar(&description, "d", "", "description to send")
 	flag.Usage = func() {
         fmt.Printf("Usage of %s:\n", os.Args[0])
-        fmt.Printf("    username password \n")
+//        fmt.Printf("    <username> <password> <description>\nor\n")
         flag.PrintDefaults()
+		fmt.Printf("example:\n")
+		fmt.Printf(" -u=test -p=test -d=\"some description\"\n")
     }
 	flag.Parse()
-	fmt.Print(user)
-	fmt.Printf("other args: %+v\n", flag.Args())
+
+	if (user == ""){
+		fmt.Printf("User undefined\n")
+		flag.PrintDefaults()
+	} else {
+		fmt.Printf("User %s", user)
+	}
+
+	if (password == ""){
+		fmt.Printf("Password undefined\n")
+	}
+
+	fmt.Printf("DEBUG: other args: %+v\n", flag.Args())
+
+
+
 }
