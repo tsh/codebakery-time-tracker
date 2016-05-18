b6efbb828185f00c3df1a18dd6f25d839cc7f521
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2016-05-18 17:31:30 +0300

    register blueprint in create_app

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,3 @@
-import os
-
 from flask import Flask, Blueprint
 from flask.ext.sqlalchemy import SQLAlchemy
 
@@ -9,12 +7,12 @@ from flask_restful import Api
 db = SQLAlchemy(session_options={'expire_on_commit': False})
 
 app = Flask(__name__)
+api_v1 = Blueprint('api_v1', 'api_v1', url_prefix='/api/v1/')
+api = Api(api_v1)
+
 
 def create_app(config_object):
     app.config.from_object(config_object)
     db.init_app(app)
+    app.register_blueprint(api_v1)
     return app
-
-api_v1 = Blueprint('api_v1', 'api_v1', url_prefix='/api/v1/')
-api = Api(api_v1)
-
--- run.py
+++ run.py
@@ -19,5 +19,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         create_admin()
-    app.register_blueprint(api_v1)
     app.run(host='0.0.0.0', debug=True, port=8000)
