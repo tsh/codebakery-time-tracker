228f21f49f43160b69baf2e980ac9281d8758591
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2016-05-19 16:29:42 +0300

    add frontent boilerplate, drop markup

---
+++ webpack.config.prod.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    './src/index',
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/',
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production'),
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+      },
+    }),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src'),
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass'],
+      },
+    ],
+  },
+  sassLoader: {
+    includePaths: [path.resolve(__dirname, 'node_modules/bootstrap/scss')],
+  },
+};
