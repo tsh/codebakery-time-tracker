9375e3494bf1bc0d83f2642e5927ac6677ae4db4
Vladimir <vladimirpovlj@gmail.com>
2016-05-25 15:48:12 +0300

    admin page layout

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -18,6 +18,10 @@ module.exports = {
         loaders: ['style', 'css', 'sass'],
         include: [path.join(__dirname, 'scss')],
       },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      }
     ],
   },
   sassLoader: {
